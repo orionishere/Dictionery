9d0e2a573f40bd652d36c9b92afae338296c287f
Abhijeet Phanse <orionishere@users.noreply.github.com>
2019-09-19 21:38:24 +0530

    Add files via upload

---
+++ Dictionary_Code.py
@@ -0,0 +1,36 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+def definition(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in  data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys()) [0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys()) [0]]
+        elif yn == "y":
+            return data[get_close_matches(word, data.keys()) [0]]
+        elif yn == "N":
+            return "This word is not present in the dictionary!"
+        elif yn == "n":
+            return "This word is not present in the dictionary!"
+        else:
+            return "We didn't recognise your answer"
+    else:
+        return "This word is not present in the dictionary!"
+
+word_input = input("Enter word: ")
+
+answer = definition(word_input)
+
+if type(answer) == list:
+    for item in answer:
+        print(item)
+else:
+    print(answer)
